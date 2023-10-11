f394a62c5f9150f676a04c414fc199e924bfd54d
StevenProgrammer01 <91440171+StevenProgrammer01@users.noreply.github.com>
2023-10-10 21:59:41 -0600

    Testing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def webhook():
     handler = APIHandler()
     # Obtiene los datos JSON de la solicitud POST
     data = request.get_json()
-    message= data["queryResult"]["queryText"]
+    message= data["text"]
 
     aiResponse = handler.ai_request(message)
     return jsonify(aiResponse)
